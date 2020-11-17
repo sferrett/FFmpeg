0c7875c5065c786dc523694bd798e4fde0245913
sferrett@livexlive.com <sferrett@livexlive.com>
2020-11-17 20:46:09 +0000

    make hls warning a debug only message

--- libavformat/hlsenc.c
+++ libavformat/hlsenc.c
@@ -2314,7 +2314,7 @@ static int hls_write_packet(AVFormatContext *s, AVPacket *pkt)
             if (pkt->duration) {
                 vs->duration += (double)(pkt->duration) * st->time_base.num / st->time_base.den;
             } else {
-                av_log(s, AV_LOG_WARNING, "pkt->duration = 0, maybe the hls segment duration will not precise\n");
+                av_log(s, AV_LOG_DEBUG, "pkt->duration = 0, maybe the hls segment duration will not precise\n");
                 vs->duration = (double)(pkt->pts - vs->end_pts) * st->time_base.num / st->time_base.den;
             }
         }
